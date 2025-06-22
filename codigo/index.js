8dbb2fa8efd96ca93a27a80c08362d8b67f83696
Matheus Ramalho <matheusgouvearamalho@gmail.com>
2025-06-22 19:40:29 -0300

    Update index.js

--- codigo/index.js
+++ codigo/index.js
@@ -8,6 +8,7 @@ const middlewares = jsonServer.defaults({ noCors: true })
 server.use(middlewares)
 server.use(router)
 
-server.listen(3000, () => {
-  console.log(`JSON Server is running em http://localhost:3000`)
-})
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on http://localhost:${port}`);
+});
