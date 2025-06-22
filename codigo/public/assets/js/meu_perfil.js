4a037cf4619ae29154a854964ac1949dfe7ada7f
Matheus Ramalho <matheusgouvearamalho@gmail.com>
2025-06-22 20:20:18 -0300

    Update meu_perfil.js 2

--- codigo/public/assets/js/meu_perfil.js
+++ codigo/public/assets/js/meu_perfil.js
@@ -1,6 +1,6 @@
 
 
-const API_BASE_URL = 'https://ti1-2025-1-t2-manha-adoteme-json.onrender.com'; // ESTE É O URL DO SEU BACKEND (JSON SERVER)
+const API_BASE_URL = 'https://ti1-2025-1-t2-manha-adoteme-p1jd.onrender.com'; 
 const API_USUARIOS_URL = `${API_BASE_URL}/usuarios`;
 const API_PETS_URL = `${API_BASE_URL}/pets`;
 
